e59602d191f320415f2a2ec6e81d3cf11596f249
agusbudionoprastyo <agna4605@gmail.com>
2024-06-11 15:54:11 +0700

    css metismenu

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -5,7 +5,7 @@
                    <img src="{{ asset('assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
                </div>
                <div>
-                   <h4 class="logo-text">dafam</h4>
+                   <h4 class="logo-text"></h4>
                </div>
                <div class="toggle-icon ms-auto"> <i class="bi bi-list"></i>
                </div>
