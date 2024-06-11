1a891dcfb7aae6b4fe6fed81979e2e9990f401fb
agusbudionoprastyo <agna4605@gmail.com>
2024-06-11 22:11:00 +0700

    del footer

---
+++ public/assets/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  // let year = new Date().getFullYear()
+  // var copyright = document.createElement('div')
+  // copyright.className = 'footer'
+  // copyright.innerHTML = '<div class="container-fluid"><div class="row"><div class="col-sm-6"> ' + year +' \xA9 dafam</div><div class="col-sm-6"><div class="text-sm-end d-none d-sm-block"> Powered <i class="mdi mdi-heart text-danger"></i> by <a href="#">Whatsapp Rest-API</a></div></div></div></div>'
+  // document.querySelector('.wrapper').appendChild(copyright)
+})
