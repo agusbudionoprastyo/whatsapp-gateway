b6a68341c7eeb656f858beb16866512653add24a
agusbudionoprastyo <agna4605@gmail.com>
2024-06-11 16:43:42 +0700

    change title

--- resources/views/components/layout-dashboard.blade.php
+++ resources/views/components/layout-dashboard.blade.php
@@ -44,7 +44,7 @@ You are not allowed to share or sell this source code without permission.
     {{-- csrf --}}
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <title>{{ $title }} | MPWA Multi device version</title>
+    <title>{{ $title }} • Whatsapp Gateway</title>
 </head>
 
 <body>
