4670b8c0280b9ab71203df4a6168cefdb7081202
agusbudionoprastyo <agna4605@gmail.com>
2024-06-11 15:10:15 +0700

    footer

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
   let year = new Date().getFullYear()
   var copyright = document.createElement('div')
   copyright.className = 'footer'
-  copyright.innerHTML = '<div class="container-fluid"><div class="row"><div class="col-sm-6"> ' + year +' \xA9 dafam</div><div class="col-sm-6"><div class="text-sm-end d-none d-sm-block"> Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="#">M Pedia</a></div></div></div></div>'
+  copyright.innerHTML = '<div class="container-fluid"><div class="row"><div class="col-sm-6"> ' + year +' \xA9 dafam</div><div class="col-sm-6"><div class="text-sm-end d-none d-sm-block"> Powered <i class="mdi mdi-heart text-danger"></i> by <a href="#">Whatsapp Rest-API</a></div></div></div></div>'
   document.querySelector('.wrapper').appendChild(copyright)
 })
